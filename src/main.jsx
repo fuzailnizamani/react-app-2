4521abc90bc19ed4bb69049f4b85bcb02c6e8d50
fuzailnizmani <fuzailnizamani13@gmail.com>
2025-03-17 02:13:59 +0500

    complete travel journal

--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,6 @@
 import {createRoot} from "react-dom/client"
 import App from "./App";
 
-const listName = ["alice", "bob", "charlie", "danielle"]
-// --> ["Alice", "Bob", "Charlie", "Danielle"] 
-
-
-const upCase = listName.map(function (name) {
-  let newName;
-  for(let i = 0; i <= name.length-1; i++){
-    if(i === 0){
-      newName = name.replace(name[i], name[i].toUpperCase());
-    }
-  }
-  return newName;
-});
-
-console.log(upCase);
-
-const pokemon = ["Bulbasaur", "Charmander", "Squirtle"]
-// -->          ["<p>Bulbasaur</p>", "<p>Charmander</p>", "<p>Squirtle</p>"]
-// Your code here
-
-const newPokemon = pokemon.map( poke => `<p>${poke}</p>`);
-
-console.log(newPokemon)
-
 const root = createRoot(document.getElementById("root"))
 root.render(
   <App />
